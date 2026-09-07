2c51659c5a862d3fd4f5f146857e77317d297228
Pratik Patil <pratikpatil00005@gmail.com>
2026-09-07 23:07:16 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,14 +1,7 @@
-def call(String sonarInstallation = 'SonarQube') {
+def call(String sonarInstallation = 'Sonar', String projectKey, String projectName) {
     stage('SonarQube Analysis') {
-        // Wraps the scanner execution with the server environment configuration
         withSonarQubeEnv(sonarInstallation) {
-            if (fileExists('pom.xml')) {
-                sh 'mvn clean verify sonar:sonar'
-            } else if (fileExists('package.json')) {
-                sh 'sonar-scanner'
-            } else {
-                error 'No supported build file found (pom.xml or package.json)'
-            }
+            sh "sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.projectName=${projectName}"
         }
     }
 }
